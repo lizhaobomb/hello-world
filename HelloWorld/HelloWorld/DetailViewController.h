7a781e8459e693940b3995e8d8b8e0f5b1910118
lizhao <lizhao@lizhaodeMac-mini.local>
2014-07-24 17:50:04 +0800

    第一次提交

---
+++ HelloWorld/HelloWorld/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  HelloWorld
+//
+//  Created by lizhao on 14-7-24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ HelloWorld/HelloWorld/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  HelloWorld
+//
+//  Created by lizhao on 14-7-24.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
